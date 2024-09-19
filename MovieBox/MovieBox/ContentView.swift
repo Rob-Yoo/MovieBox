1d353db6eb6c025de78c2b449a12d304c20af7c7
Rob-Yoo <jin12243@naver.com>
2024-09-19 16:28:17 +0900

    Chore: 프로젝트 환경 설정 및 라이브러리 추가

---
+++ MovieBox/MovieBox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieBox
+//
+//  Created by Jinyoung Yoo on 9/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovieBox/MovieBox/MovieBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBoxApp.swift
+//  MovieBox
+//
+//  Created by Jinyoung Yoo on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
